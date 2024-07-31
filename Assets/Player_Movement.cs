87bd0ab33955ef9feec1d32d224f3af2c81c46ea
Bryson Lindsley <brysonlindsley@icloud.com>
2024-07-31 16:22:34 -0700

    Wario

--- Assets/Player_Movement.cs
+++ Assets/Player_Movement.cs
@@ -1,28 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEditor.PlayerSettings;
 
 public class Player_Movement : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float jumpForce;
     Vector3 pos;
+    private int playerDirection;
+    private Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody = GetComponent<Rigidbody2D>();
+        playerDirection = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        pos = transform.position;
-        if (Input.GetKeyDown(KeyCode.D))
+        playerDirection = 0;
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.position = new Vector3(pos.x + speed, pos.y, pos.z);
+            playerDirection++;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.position = new Vector3(pos.x + speed, pos.y, pos.z);
+            playerDirection--;
+        }
+        rigidbody.AddForce(Vector3.right * speed * playerDirection);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce);
         }
     }
 }
